117e2740eb733c2361ade61596ae3f056eb893bb
gami24 <gami24@users.noreply.github.com>
2026-01-13 05:04:44 +0000

    修正: マイグレーション0008のカラム名を正しく更新（instagram_handle, tiktok_handle, twitter_handle）

--- migrations/0008_add_user_sns_info.sql
+++ migrations/0008_add_user_sns_info.sql
@@ -1,8 +1,5 @@
 -- Add user SNS information columns
-ALTER TABLE users ADD COLUMN twitter_url TEXT;
-ALTER TABLE users ADD COLUMN instagram_url TEXT;
 ALTER TABLE users ADD COLUMN youtube_url TEXT;
-ALTER TABLE users ADD COLUMN tiktok_url TEXT;
-ALTER TABLE users ADD COLUMN threads_url TEXT;
-ALTER TABLE users ADD COLUMN note_url TEXT;
-ALTER TABLE users ADD COLUMN other_sns_url TEXT;
+ALTER TABLE users ADD COLUMN instagram_handle TEXT;
+ALTER TABLE users ADD COLUMN tiktok_handle TEXT;
+ALTER TABLE users ADD COLUMN twitter_handle TEXT;
